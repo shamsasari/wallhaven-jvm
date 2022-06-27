e82b6e5e6145bcc652ed9de1323cb4e067e5866d
Shams Asari <shams.asari@gmail.com>
2022-06-27 07:58:08 +0100

    Added excludeSimilarTags config and changed config to JSON

--- src/main/kotlin/shamsasari/wallhavenplugin/WallhavenPlugin.kt
+++ src/main/kotlin/shamsasari/wallhavenplugin/WallhavenPlugin.kt
@@ -1,5 +1,6 @@
 package shamsasari.wallhavenplugin
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.json.JsonMapper
 import org.apache.logging.log4j.LogManager
 import java.awt.GraphicsEnvironment
@@ -12,11 +13,13 @@ import java.net.http.HttpResponse.BodyHandlers
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.*
 import java.util.concurrent.CompletableFuture
 import kotlin.io.path.*
 
 object WallhavenPlugin {
+    private val logger by lazy { LogManager.getLogger("MainLog") }
+    private val jsonMapper = JsonMapper()
+
     @JvmStatic
     fun main(args: Array<String>) {
         val appHomeDir = Path.of(System.getProperty("user.home")) / "wallhaven-plugin"
@@ -24,34 +27,24 @@ object WallhavenPlugin {
 
         System.setProperty("app.home", appHomeDir.toString())
 
-        val config = Properties()
-        val configFile = appHomeDir / "wallhaven-plugin.properties"
-        if (configFile.exists()) {
-            configFile.inputStream().use(config::load)
+        val configFile = appHomeDir / "wallhaven-plugin.json"
+        val config = if (configFile.exists()) {
+            jsonMapper.readTree(configFile.toFile())
         } else {
             configFile.createFile()
+            jsonMapper.createObjectNode()
         }
 
         val httpClient = HttpClient.newHttpClient()
-        val displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.displayMode
 
-        val randomWallpapersUrl = buildString {
-            append("https://wallhaven.cc/api/v1/search?resolutions=")
-                .append(displayMode.width).append('x').append(displayMode.height)
-                .append("&sorting=random")
-            val q = config.getProperty("q")
-            if (q != null) {
-                append("&q=").append(URLEncoder.encode(q, UTF_8))
-            }
+        val matchingWallpaper = findMatchingWallpaper(config, httpClient)
+        if (matchingWallpaper == null) {
+            logger.warn("No results")
+            return
         }
 
-        val mapper = JsonMapper()
-        val response = httpClient.get(randomWallpapersUrl).use(mapper::readTree)
-
-        val firstWallpaper = response["data"][0]
-        println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(firstWallpaper))
-        val id = firstWallpaper["id"].textValue()
-        val wallpaperUrl = firstWallpaper["path"].textValue()
+        val id = matchingWallpaper["id"].textValue()
+        val wallpaperUrl = matchingWallpaper["path"].textValue()
 
         val setWallpaperFuture = CompletableFuture.runAsync {
             val wallpaperFile = Files.createTempDirectory("wallhaven-plugin") / id
@@ -59,20 +52,63 @@ object WallhavenPlugin {
             WindowsOperatingSystem.setWallpaper(wallpaperFile)
         }
 
-        val tags = httpClient.get("https://wallhaven.cc/api/v1/w/$id").use(mapper::readTree)["data"]["tags"]
-        val tagsString = tags.joinToString(prefix = "[", postfix = "]") {
+        val tagsString = httpClient.getWallpaperTags(id).joinToString(prefix = "[", postfix = "]") {
             "${it["name"].textValue()} (${it["id"].intValue()})"
         }
 
-        val wallhavenUrl = firstWallpaper["url"]
-        val total = response["meta"]["total"].intValue()
-
-        LogManager.getLogger("MainLog").info("$wallhavenUrl from $total matching wallpapers $tagsString")
+        logger.info("${matchingWallpaper["url"]} $tagsString")
 
         setWallpaperFuture.join()
     }
 
+    private fun findMatchingWallpaper(config: JsonNode, httpClient: HttpClient): JsonNode? {
+        val displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.displayMode
+
+        val q = config["q"]?.textValue()?.let { URLEncoder.encode(it, UTF_8) }
+        val excludeSimilarTags = config["excludeSimilarTags"]?.mapTo(HashSet()) { it.textValue().lowercase() } ?: emptySet()
+
+        var page = 0
+        while (true) {
+            page++
+            val randomWallpapersUrl = buildString {
+                append("https://wallhaven.cc/api/v1/search?resolutions=")
+                    .append(displayMode.width).append('x').append(displayMode.height)
+                    .append("&sorting=random")
+                    .append("&page=").append(page)
+                if (q != null) {
+                    append("&q=").append(URLEncoder.encode(q, UTF_8))
+                }
+            }
+
+            val response = httpClient.getJson(randomWallpapersUrl)
+
+            val data = response["data"] ?: return null
+
+            if (excludeSimilarTags.isEmpty()) {
+                return data[0]
+            } else {
+                for (json in data) {
+                    val id = json["id"].textValue()
+                    val matching = httpClient.getWallpaperTags(id).all { tag ->
+                        tag["name"].textValue().lowercase() !in excludeSimilarTags
+                    }
+                    if (matching) {
+                        return json
+                    }
+                    logger.info("${json["url"]} does not match")
+                }
+                logger.info("No matching wallpaper, searching on next page...")
+            }
+        }
+    }
+
+    private fun HttpClient.getWallpaperTags(id: String): JsonNode = getWallpaperInfo(id)["data"]["tags"]
+
+    private fun HttpClient.getWallpaperInfo(id: String): JsonNode = getJson("https://wallhaven.cc/api/v1/w/$id")
+
     private fun HttpClient.get(url: String): InputStream {
         return send(HttpRequest.newBuilder(URI(url)).GET().build(), BodyHandlers.ofInputStream()).body()
     }
+
+    private fun HttpClient.getJson(url: String): JsonNode = get(url).use(jsonMapper::readTree)
 }
